2a030899b6514cfff47d7dd241df30427afdcf44
igormuba <igorm-uba@hotmail.com>
2018-12-31 19:27:42 -0200

    ERC20 simple implementation

--- FirstClass.sol
+++ FirstClass.sol
@@ -6,27 +6,39 @@ provider = new Web3.providers.HttpProvider("http://localhost:8545")
 web3 = new Web3(provider)
 
 contract firstClassContract{
+
+    string public _tokenName;
+    string public _tokenSymbol;
     address public creator; //address of contract creator to be defined
-    uint256 public totalSupply; //total coin supply
+    uint256 public _totalSupply; //total coin supply
+    mapping (address => mapping (address => uint256)) private _allowed; //allowance
     mapping (address => uint256) public balances; //like a dictionary, an address represents
                                                   // a positive integer
     // new variables can't be created
     //the above ones are created during contract creation
 
     //below is the constructor function called only when the contract is created
-    function firstClassContract() public{
+    constructor() public{
+        _tokenName = "ERC20 Token"; //sets the name of the token
+        _tokenSymbol = "ERCT"; //ticker symbol of the token
         creator = msg.sender; //the creator of the contract
         //msg.sender is automatically generated on contract deploy
-        totalSupply = 100; //sets the total supply
-        balances[creator] = totalSupply; //gives all the supply to the contract creator
+        _totalSupply = 100; //sets the total supply
+        balances[creator] = _totalSupply; //gives all the supply to the contract creator
     }
 
-    function balanceOf(address memory owner) public view returns(uint256 memory){
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function balanceOf(address owner) public view returns(uint256){
         return balances[owner];
     }
 
+    function allowance(address owner, address spender) public view returns (uint256){
+        return _allowed[owner][spender]; //adds to the allowance mapping
+    }
 
-    function sendTokens(address memory receiver, uint256 memory amount) 
+    function transfer(address receiver, uint256 amount) 
         public returns(bool){
             address owner = msg.sender; //this is the caller of the function
             
@@ -37,16 +49,28 @@ contract firstClassContract{
             balances[receiver] += amount; //add to the balance of the receiver after
             return true;
         }
+         function transferFrom(address sender, address receiver, uint256 amount) 
+        public returns(bool){
+            require(amount <= _allowed[sender][msg.sender]); //requires that the caller of the function has the permission to send this value from the sender of the amount
+            address owner = msg.sender; //this is the caller of the function
+            
+            require(amount > 0); //the caller has to have a balance of more than zero tokens
+            require(balances[sender] >= amount); //the balance must be bigger than the transaction amount
+            
+            balances[sender] -= amount; //deduct from the balance of the sender first
+            balances[receiver] += amount; //add to the balance of the receiver after
+            return true;
+        }
+
+        function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 
-    function setStringVariable(string memory newVariable) public {
-        stringVariable = newVariable;
-    }
 
-    function getStringVariable() public view returns (string memory){
-        return stringVariable;
-        //test visual studio version control
-    }
 
-}
+    }
+   
+
+
+    
 
